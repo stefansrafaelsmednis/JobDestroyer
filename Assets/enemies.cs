28977bbcc4fd01322f701a371409ce2db6e9e12e
Edi <murnieksedvards@gmail.com>
2025-04-28 16:54:47 +0300

    creating enemie AI

--- Assets/enemies.cs
+++ Assets/enemies.cs
@@ -1,6 +1,37 @@
+using UnityEngine.AI;
 using UnityEngine;
 
-public class enemies
+public class enemies : MonoBehaviour
 {
-    
+    public NavMeshAgent agent:
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttackted;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playernAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find(player).transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Patroling()
+    {
+
+    }
+    private void patroling()
+
 }
